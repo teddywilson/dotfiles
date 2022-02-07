946251d473e56aa1090c3167938e59420ae018d6
Teddy Wilson <hello@teddywilson.me>
2022-02-06 20:07:12 -0500

    Update git.zsh

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,4 @@
+alias auth="git credential-osxkeychain erase && gh auth login"
 alias gfa="git fetch --all && git remote update"
 alias ghp="gfa && git checkout origin/gh-pages"
-alias master="gfa && git checkout origin/master"
+alias main="gfa && git checkout origin/main"
