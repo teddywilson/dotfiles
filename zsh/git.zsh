6bbd330c7464d5addf7c212f96861223d13761f7
teddywilson <hello@teddywilson.me>
2022-02-09 18:21:47 -0500

    auth -> gauth

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,4 +1,4 @@
-alias auth="git credential-osxkeychain erase && gh auth login"
+alias gauth="git credential-osxkeychain erase && gh auth login"
 alias gfa="git fetch --all && git remote update"
 alias ghp="gfa && git checkout origin/gh-pages"
 alias main="gfa && git checkout origin/main"
